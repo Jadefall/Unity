3a5acb7407fcb4945dcf310bff56669ed6b18736
Lokylicious <bch.kraaij@gmail.com>
2020-05-20 01:42:47 +0200

    Player Prefab
    Bijna vergeten lmao

--- Jadefall/Assets/Player Controller/Scripts/PlayerHealth.cs
+++ Jadefall/Assets/Player Controller/Scripts/PlayerHealth.cs
@@ -6,14 +6,6 @@ public class PlayerHealth : MonoBehaviour
 {
     public int health = 100;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-            health = 0;
-        }
-    }
-
     void LateUpdate()
     {
         if (health <= 0)
