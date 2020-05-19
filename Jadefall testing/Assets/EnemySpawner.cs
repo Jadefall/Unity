cb8373bfd2d0ac2125b2a0a19d3b01682612a951
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2020-05-19 21:03:51 +0200

    scripts voor meuk begin dingen

---
+++ Jadefall testing/Assets/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public List<GameObject> list = new List<GameObject>();
+    public GameObject golem;
+    public Vector3 spawnPoint;
+
+    public int totalGolems;
+    public int spawnedGolems;
+
+    public float waveTimer;
+    public float timeTillWave;
+
+    public void Wave()
+    {
+        Instantiate(golem, spawnPoint, Quaternion.identity);
+    }
+
+}
