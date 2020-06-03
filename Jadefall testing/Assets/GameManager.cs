e7bb583ebc952cb5ad566b87526d50591889bdd2
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2020-06-03 11:08:39 +0200

    gameManager met wavespawner
    s

---
+++ Jadefall testing/Assets/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject golem;
+    public Vector3 spawnPoint;
+
+    public int noGolems = 0;
+    public int spawnedGolems;
+    public int maxGolems;
+
+    public List<float> list = new List<float>();
+    public float waveTimer;
+    public float timeTillWave;
+    public float totalWaves;
+
+    public bool spawn;
+
+    private void Update()
+    {
+        if(spawnedGolems == noGolems)
+        {
+            wave();
+        }
+        if(spawnedGolems == maxGolems)
+        {
+            spawn = false;
+        }
+    }
+    
+    public void wave()
+    {
+        if (spawn == true)
+        {
+            if (spawnedGolems == 0)
+            {
+                if (waveTimer == 0)
+                {
+                    spawnGolem();
+                }
+            }
+        }
+    }
+
+    private void spawnGolem()
+    {
+        Instantiate(golem, spawnPoint, Quaternion.identity);
+        spawnedGolems++;
+    }
+
+    public void killGolem()
+    {
+        spawnedGolems--;
+    }
+}
