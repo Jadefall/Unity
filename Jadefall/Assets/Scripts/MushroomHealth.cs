ec4179dd0d44134600a5e46673f125b523adb1e9
Dostac <maxknol@live.nl>
2020-06-09 19:11:27 +0200

    Wind_HeathBar_PlayArea

---
+++ Jadefall/Assets/Scripts/MushroomHealth.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MushroomHealth : MonoBehaviour
+
+
+{
+
+    public Color[] color;
+    public Image hpImg;
+    public PlayerHealth playerHealth;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float f = playerHealth.health / 100f;
+        hpImg.fillAmount = f;
+        if (f > 0.83)
+        {
+            hpImg.color = color[0];
+        }
+        else if (f > 0.66)
+        {
+            hpImg.color = color[1];
+        }
+        else if (f > 0.49)
+        {
+            hpImg.color = color[2];
+        }
+        else if (f > 0.32)
+        {
+            hpImg.color = color[3];
+        }
+        else if (f > 0.15)
+        {
+            hpImg.color = color[4];
+        }
+        else if (f > 0.01)
+        {
+            hpImg.color = color[5];
+        }
+
+
+
+    }
+}
---
+++ Jadefall/Assets/Scripts/PlayArea.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayArea : MonoBehaviour
+{
+    public GameObject canvas;
+
+    private void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.transform.tag == "PlaySphere")
+        {
+            canvas.SetActive(false);
+            print("player has re-entered the play area!");
+        }
+        
+    }
+
+    public void OnTriggerExit(Collider col)
+    {
+        if (col.transform.tag == "PlaySphere")
+        {
+            canvas.SetActive(true);
+            print("player has left the play area!");
+        }
+
+    }
+}
