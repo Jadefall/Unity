41489b4a79ce9f7594e04b8916868153a30d8b95
Dostac <maxknol@live.nl>
2020-06-11 11:14:42 +0200

    HouseHealthUI

---
+++ Jadefall/Assets/Scripts/HouseHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseHealth : MonoBehaviour
+{
+    public int health = 500;
+
+    void LateUpdate()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Jadefall/Assets/Scripts/HouseHealthMeter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HouseHealthMeter : MonoBehaviour
+{
+
+    public Color[] color;
+    public Image hpImg;
+    public HouseHealth HouseHealth;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float f = HouseHealth.health / 500f;
+        hpImg.fillAmount = f;
+        if (f > 0.83)
+        {
+            hpImg.color = color[0];
+        }
+        else if (f > 0.66)
+        {
+            hpImg.color = color[1];
+        }
+        else if (f > 0.49)
+        {
+            hpImg.color = color[2];
+        }
+        else if (f > 0.32)
+        {
+            hpImg.color = color[3];
+        }
+        else if (f > 0.15)
+        {
+            hpImg.color = color[4];
+        }
+        else if (f > 0.01)
+        {
+            hpImg.color = color[5];
+        }
+
+
+
+    }
+}
